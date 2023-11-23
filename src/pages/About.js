3ab3db1bbcade44e0f7e19fde15c876bef90b82e
Dineshkumar sarangapani <dineshkumar_sarangapani@trimble.com>
2023-11-23 13:43:12 +0530

    Adding snackbar

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,53 @@
-import React from 'react'
-import Grid from '@mui/material/Grid';
+import React, { useState } from 'react'
+import CommonButton from "../components/common/CommonButton/CommonButton";
+import GridWrapper from "../components/common/GridWrapper/GridWrapper";
+import BasicSnackbar from "../components/common/BasicSnackbar/BasicSnackbar";
 
-const About = () => {
+const Storage = () => {
+    const [open, setOpen] = useState(false);
+    const [severity, setSeverity] = useState('success');
 
+    const handleClick = () => {
+        setOpen(true);
+        setSeverity("success")
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+        // setSeverity(false)
+    };
+
+    const handleFailureClick = () => {
+        setOpen(true);
+        setSeverity("error")
+    };
     return (
-        <Grid item xs={8}>
-            This is Solutions page.
-        </Grid>
+        <GridWrapper>
+            This is storage page.
+            <CommonButton
+                variant="contained"
+                onClick={handleClick}
+            >
+                Open success snackbar
+            </CommonButton>
+            <CommonButton
+                variant="contained"
+                onClick={handleFailureClick}
+            >
+                Open Failure snackbar
+            </CommonButton>
+            <BasicSnackbar
+                open={open}
+                onClose={handleClose}
+                severity={severity}
+                message="Error msg"
+            />
+
+        </GridWrapper>
     )
 }
 
-export default About
+export default Storage
